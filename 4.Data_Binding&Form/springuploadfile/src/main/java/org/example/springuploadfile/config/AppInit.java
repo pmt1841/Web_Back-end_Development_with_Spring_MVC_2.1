2139653167d82f9f96b719cad909e4f25d123ff2
pmt1841 <phamminhtien1841@gmail.com>
2026-02-06 15:50:18 +0700

    create

---
+++ 4.Data_Binding&Form/springuploadfile/src/main/java/org/example/springuploadfile/config/AppInit.java
@@ -0,0 +1,27 @@
+package org.example.springuploadfile.config;
+
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement("", 52428800, 52428800, 0));
+    }
+}

55be0e5d810def493a417d0b43b2739649cacea1
pmt1841 <phamminhtien1841@gmail.com>
2026-02-06 14:29:34 +0700

    create

---
+++ 4.Data_Binding&Form/spring-user-model/src/main/java/org/example/springusermodel/config/AppInit.java
@@ -0,0 +1,20 @@
+package org.example.springusermodel.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
